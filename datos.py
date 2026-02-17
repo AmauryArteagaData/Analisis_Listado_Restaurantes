ddfe61177f1e92999128fe100e20cbf30ad27b0f
Amaury Arteaga G <amauryarteagag@MacBook-Air-de-Amaury.local>
2026-02-17 12:56:45 -0500

    Add changes

--- datos.py
+++ datos.py
@@ -5,4 +5,26 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("restaurantes_la_dorada_caldas.csv")
 
 #Mostrar las primeras filas del dataframe
-print(df.head())
+print(df.head()), 
+print("\n"),
+print(df.info()),
+print("\n"),
+print(df.describe()),
+print("\n"),
+print(df.isnull().sum())
+
+#Convertir nombres de columnas a minúsculas y reemplazar espacios por guiones bajos
+df.columns = df.columns.str.lower().str.replace(' ', '_')
+
+#Identificar las columnas que tengan valores nulos
+nulos = df.isnull().sum()
+
+#Porcentaje de nulos
+porcentaje_nulos = (df.isnull().mean() * 100).round(2)
+
+#Resumen de las columnas con nulos
+resumen_nulos = pd.DataFrame({
+    "nulos": nulos,
+    "porcentaje_nulos": porcentaje_nulos
+}).sort_values(by = "porcentaje_nulos", ascending=False)
+print(resumen_nulos)
